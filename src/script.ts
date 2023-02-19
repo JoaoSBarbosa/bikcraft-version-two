d2b07afa26d8328c548852469e348e76ff65f705
João Uninter <contato.jsbarbosa@gmail.com>
2023-02-19 10:58:59 -0300

    Mudanças finais e implementação de acessibilidade

--- src/script.ts
+++ src/script.ts
@@ -1,13 +1,19 @@
-const btnMobile = document.getElementById('btn-mobile') as HTMLElement;
+const btnMobile = document.getElementById("btn-mobile") as HTMLElement;
 
-btnMobile.addEventListener('click', toggleMenu);
-btnMobile.addEventListener('touchstart', toggleMenu);
+btnMobile.addEventListener("click", toggleMenu);
+btnMobile.addEventListener("touchstart", toggleMenu);
 
-
-function toggleMenu(event: any){
-  if(event.type === 'touchstart'){
+function toggleMenu(event: any) {
+  if (event.type === "touchstart") {
     event.preventDefault();
   }
-  const nav = document.getElementById('nav') as HTMLElement;
-  nav.classList.toggle('active');
+  const nav = document.getElementById("nav") as HTMLElement;
+  nav.classList.toggle("active");
+  const active = nav.classList.contains("active");
+  event.currentTarget.setAttribute("aria-expanded", active);
+  if (active) {
+    event.currentTarget.setAttribute("aria-label", "Fechar Menu");
+  } else {
+    event.currentTarget.setAttribute("aria-label", "Abrir Menu");
+  }
 }
